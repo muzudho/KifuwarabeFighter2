0497cbed2761982d81d262149ac61a758cfa5165
muzudho <muzudho1@gmail.com>
2017-01-24 19:38:15 +0900

    parser (incomplete)

--- KifuwarabeFighter2/Assets/Editor/Util_StellaQL.cs
+++ KifuwarabeFighter2/Assets/Editor/Util_StellaQL.cs
@@ -18,41 +18,60 @@ namespace StellaQL
             List<int> result = new List<int>();
             List<string> tokens = new List<string>();
 
+            // 字句解析
             {
-                StringBuilder sb = new StringBuilder();
                 Util_StellaQL.String_to_tokens(expression, tokens);
-                foreach (string token in tokens)
-                {
-                    sb.Append("token: ");
-                    sb.AppendLine(token);
-                }
-                Debug.Log( sb.ToString() );
+                //StringBuilder sb = new StringBuilder();
+                //foreach (string token in tokens)
+                //{
+                //    sb.Append("token: ");
+                //    sb.AppendLine(token);
+                //}
+                //Debug.Log( sb.ToString() );
             }
 
             Stack<char> stack = new Stack<char>();
 
-            int caret = 0;
-            switch (expression[caret])
+            // スタック積み
             {
-                case ' ':
-                    caret++;
-                    break;
-                case '(':
-                    stack.Push('(');
-                    break;
-                case '[':
-                    break;
-                case '{':
-                    break;
-                case ')':
-                    break;
-                case ']':
-                    break;
-                case '}':
-                    break;
-                default:
-                    break;
+                string openParen = ""; // 閉じ括弧に対応する、「開きカッコ」
+                int iCursor = 0;
+                while ( iCursor < tokens.Count )
+                {
+                    string token = tokens[iCursor];
+                    if (""== openParen)
+                    {
+                        StringBuilder sb1 = new StringBuilder();
+                        sb1.Append("go["); sb1.Append(iCursor); sb1.Append("]: "); sb1.Append(token); sb1.AppendLine();
+                        Debug.Log(sb1.ToString());
+                        switch (token)
+                        {
+                            case ")": openParen = "("; tokens[iCursor] = ""; break;
+                            case "]": openParen = "["; tokens[iCursor] = ""; break;
+                            case "}": openParen = "{"; tokens[iCursor] = ""; break;
+                            default: break; // 無視して進む
+                        }
+                    }
+                    else // 後ろに進む☆
+                    {
+                        StringBuilder sb2 = new StringBuilder();
+                        sb2.Append("back["); sb2.Append(iCursor); sb2.Append("]: "); sb2.Append(token); sb2.AppendLine();
+                        Debug.Log(sb2.ToString());
+                        switch (token)
+                        {
+                            case "(":
+                            case "[":
+                            case "{": if (openParen == token) { openParen = ""; } break;
+                            default: break;
+                        }
+                        tokens[iCursor] = ""; // 後ろに進んだ先では、その文字を削除する
+                    }
+
+                    if ("" == openParen) { iCursor++; }
+                    else { iCursor--; }
+                }
             }
+
 
             return result;
         }
@@ -91,5 +110,7 @@ namespace StellaQL
                 word.Length = 0;
             }
         }
+
+
     }
 }
